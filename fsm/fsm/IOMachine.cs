5fe524750a21a16b77be1b45af2abe03893212ff
binczycki.t@gmail.com <binczycki.t@gmail.com@3c310c29-e50f-3924-cbbe-cfeed22d5fb8>
2011-05-30 12:30:13 +0000

    próba dodania wczytywania/ zapisywania pliku

---
+++ fsm/fsm/IOMachine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace fsm {
+		static class IOMachine {
+				static string errorMessage ="Błąd w linii {0}:"+Environment.NewLine+"{1)";
+				internal static string stringToSpace(this string s) {
+						if (s.IndexOf(" ")!= -1) return s.Substring(0, s.IndexOf(" "));
+						return s;
+				}
+				internal static string readToSpace(this string s) {
+						try{
+								return s.stringToSpace();
+						} finally {
+//nie wiem czy dziala
+								s = s.Substring(s.IndexOf(" "), s.Length); 
+						}
+				}
+				public static FunkcjaPrzejscia LoadMachine(TextReader reader) {
+						FunkcjaPrzejscia ret = new FunkcjaPrzejscia("tempName", "");
+						string s;
+						using (reader) {
+								s = reader.ReadLine();
+								if (s != "MM_TB_TCS") throw new Exception(string.Format(errorMessage, 1, "Opis Maszyny musi rozpoczynać się od napisu MM_TB_TCS"));
+								s = reader.ReadLine();
+								ret.nazwa = s.stringToSpace();
+						}
+						throw new Exception("jeszcze nie dziala. trzeba zaklepac");
+						return ret;
+				}
+				public static string SaveMachine(FunkcjaPrzejscia fP) {
+						throw new Exception("jeszcze nie dziala. trzeba zaklepac");
+						return null;
+				}
+		}
+}
