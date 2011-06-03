a4e033883d65c7b1a4a7746c510a91a2a1c7ac21
binczycki.t@gmail.com <binczycki.t@gmail.com@3c310c29-e50f-3924-cbbe-cfeed22d5fb8>
2011-06-03 21:28:45 +0000

    uregulowanie wyświetlania okien dialogowych, uzupełnienie TODO

--- fsm/fsm/RemoveDialog.Designer.cs
+++ fsm/fsm/RemoveDialog.Designer.cs
@@ -23,66 +23,66 @@
 				/// the contents of this method with the code editor.
 				/// </summary>
 				private void InitializeComponent() {
-						this.label1 = new System.Windows.Forms.Label();
-						this.listBox1 = new System.Windows.Forms.ListBox();
-						this.removeButton = new System.Windows.Forms.Button();
-						this.MyCancelButton = new System.Windows.Forms.Button();
-						this.SuspendLayout();
-						// 
-						// label1
-						// 
-						this.label1.AutoSize = true;
-						this.label1.Location = new System.Drawing.Point(54, 9);
-						this.label1.Name = "label1";
-						this.label1.Size = new System.Drawing.Size(108, 13);
-						this.label1.TabIndex = 0;
-						this.label1.Text = "Wybierz do usuniecia";
-						// 
-						// listBox1
-						// 
-						this.listBox1.FormattingEnabled = true;
-						this.listBox1.Location = new System.Drawing.Point(57, 25);
-						this.listBox1.Name = "listBox1";
-						this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
-						this.listBox1.Size = new System.Drawing.Size(162, 134);
-						this.listBox1.TabIndex = 1;
-						// 
-						// removeButton
-						// 
-						this.removeButton.Location = new System.Drawing.Point(144, 170);
-						this.removeButton.Name = "removeButton";
-						this.removeButton.Size = new System.Drawing.Size(75, 23);
-						this.removeButton.TabIndex = 2;
-						this.removeButton.Text = Language.lang[35];
-						this.removeButton.UseVisualStyleBackColor = true;
-						this.removeButton.Click += new System.EventHandler(this.button1_Click);
-						// 
-						// MyCancelButton
-						// 
-						this.MyCancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-						this.MyCancelButton.Location = new System.Drawing.Point(57, 170);
-						this.MyCancelButton.Name = "MyCancelButton";
-						this.MyCancelButton.Size = new System.Drawing.Size(75, 23);
-						this.MyCancelButton.TabIndex = 3;
-						this.MyCancelButton.Text = Language.lang[23];
-						this.MyCancelButton.UseVisualStyleBackColor = true;
-						// 
-						// RemoveDialog
-						// 
-						this.AcceptButton = this.removeButton;
-						this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-						this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-						this.CancelButton = this.MyCancelButton;
-						this.ClientSize = new System.Drawing.Size(284, 205);
-						this.Controls.Add(this.MyCancelButton);
-						this.Controls.Add(this.removeButton);
-						this.Controls.Add(this.listBox1);
-						this.Controls.Add(this.label1);
-						this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-						this.Name = "RemoveDialog";
-						this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-						this.ResumeLayout(false);
-						this.PerformLayout();
+					this.label1 = new System.Windows.Forms.Label();
+					this.listBox1 = new System.Windows.Forms.ListBox();
+					this.removeButton = new System.Windows.Forms.Button();
+					this.MyCancelButton = new System.Windows.Forms.Button();
+					this.SuspendLayout();
+					// 
+					// label1
+					// 
+					this.label1.AutoSize = true;
+					this.label1.Location = new System.Drawing.Point(54, 9);
+					this.label1.Name = "label1";
+					this.label1.Size = new System.Drawing.Size(108, 13);
+					this.label1.TabIndex = 0;
+					this.label1.Text = "Wybierz do usuniecia";
+					// 
+					// listBox1
+					// 
+					this.listBox1.FormattingEnabled = true;
+					this.listBox1.Location = new System.Drawing.Point(57, 25);
+					this.listBox1.Name = "listBox1";
+					this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+					this.listBox1.Size = new System.Drawing.Size(162, 134);
+					this.listBox1.TabIndex = 1;
+					// 
+					// removeButton
+					// 
+					this.removeButton.Location = new System.Drawing.Point(144, 170);
+					this.removeButton.Name = "removeButton";
+					this.removeButton.Size = new System.Drawing.Size(75, 23);
+					this.removeButton.TabIndex = 2;
+					this.removeButton.Text = "Usun";
+					this.removeButton.UseVisualStyleBackColor = true;
+					this.removeButton.Click += new System.EventHandler(this.button1_Click);
+					// 
+					// MyCancelButton
+					// 
+					this.MyCancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+					this.MyCancelButton.Location = new System.Drawing.Point(57, 170);
+					this.MyCancelButton.Name = "MyCancelButton";
+					this.MyCancelButton.Size = new System.Drawing.Size(75, 23);
+					this.MyCancelButton.TabIndex = 3;
+					this.MyCancelButton.Text = "Anuluj";
+					this.MyCancelButton.UseVisualStyleBackColor = true;
+					// 
+					// RemoveDialog
+					// 
+					this.AcceptButton = this.removeButton;
+					this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+					this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+					this.CancelButton = this.MyCancelButton;
+					this.ClientSize = new System.Drawing.Size(284, 205);
+					this.Controls.Add(this.MyCancelButton);
+					this.Controls.Add(this.removeButton);
+					this.Controls.Add(this.listBox1);
+					this.Controls.Add(this.label1);
+					this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+					this.Name = "RemoveDialog";
+					this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+					this.ResumeLayout(false);
+					this.PerformLayout();
 
 				}
 
