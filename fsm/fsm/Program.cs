ec4316585395b4f5c1f633a145b1cce8f4f31211
binczycki.t@gmail.com <binczycki.t@gmail.com@3c310c29-e50f-3924-cbbe-cfeed22d5fb8>
2011-06-06 13:40:57 +0000

    drobne poprawki tekstowe

--- fsm/fsm/Program.cs
+++ fsm/fsm/Program.cs
@@ -1,22 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
 
-namespace fsm
-{
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-						Language.SetPolish();
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
-        }
-    }
+namespace fsm {
+		static class Program {
+				/// <summary>
+				/// The main entry point for the application.
+				/// </summary>
+				[STAThread]
+				static void Main() {
+						Application.EnableVisualStyles();
+						Application.SetCompatibleTextRenderingDefault(false);
+						Application.Run(new Main());
+				}
+		}
 }
